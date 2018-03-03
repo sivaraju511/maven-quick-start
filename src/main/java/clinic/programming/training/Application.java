b4fbfd14f6ed48abf0799b5d78da1321fea9f83c
sivaraju <sivaraju@abcd>
2018-03-04 00:39:15 +0530

    Added dependency for commons lang3 StringUtils

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,8 +2,13 @@ package clinic.programming.training;
 
 import java.util.List;
 import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
+	public int countWords(String string){
+		String[] seperatedWords=StringUtils.split(string,' ');
+		return (seperatedWords==null)?0:seperatedWords.length;
+	}
     public void greet(){
 		List<String> greetings=new ArrayList<>();
 		greetings.add("Hello");
@@ -22,5 +27,7 @@ public class Application {
     	System.out.println ("Starting Application");
 		Application app = new Application();
 		app.greet();
+		String s="I have four words";
+		System.out.println("Word count : "+app.countWords(s));
     }
 }
